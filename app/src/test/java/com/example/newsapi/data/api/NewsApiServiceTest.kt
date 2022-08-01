e94c2d7aab73a64e4ebf4c94b440de441d8f48d3
Qui Van <quivan3396@yahoo.com>
2022-08-01 12:08:38 -0400

    add json and testing

---
+++ app/src/test/java/com/example/newsapi/data/api/NewsApiServiceTest.kt
@@ -0,0 +1,78 @@
+package com.example.newsapi.data.api
+
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.runBlocking
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import okio.buffer
+import okio.source
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class NewsApiServiceTest {
+
+    private lateinit var service: NewsApiService
+    private lateinit var server: MockWebServer
+
+    @Before
+    fun setUp() {
+        server = MockWebServer()
+        service = Retrofit.Builder()
+            .baseUrl(server.url(""))
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(NewsApiService::class.java)
+    }
+
+    private fun enqueueMockResponse(fileName: String) {
+        val inputStream = javaClass.classLoader!!.getResourceAsStream(fileName)
+        val source = inputStream.source().buffer()
+        val mockResponse = MockResponse()
+
+        mockResponse.setBody(source.readString(Charsets.UTF_8))
+        server.enqueue(mockResponse)
+
+    }
+
+    @Test
+    fun getTopHeadlines_sentRequest_receivedExpected() {
+        runBlocking {
+            enqueueMockResponse("newsresponse.json")
+            val responseBody = service.getTopHeadlines("us", 1).body()
+            val request = server.takeRequest()
+            assertThat(responseBody).isNotNull()
+            assertThat(request.path).isEqualTo("/v2/top-headlines?country=us&page=1&apiKey=dbfd4ede6cb041b3a21ffe227d301cbf")
+        }
+    }
+
+    @Test
+    fun getTopHeadlines_receivedResponse_correctPageSize() {
+        runBlocking {
+            enqueueMockResponse("newsresponse.json")
+            val responseBody = service.getTopHeadlines("us", 1).body()
+            val articleList = responseBody!!.articles
+            assertThat(articleList.size).isEqualTo(3)
+        }
+    }
+
+    @Test
+    fun getTopHeadlines_receivedResponse_correctContent() {
+        runBlocking {
+            enqueueMockResponse("newsresponse.json")
+            val responseBody = service.getTopHeadlines("us", 1).body()
+            val articleList = responseBody!!.articles
+            val article = articleList[0]
+            assertThat(article.author).isEqualTo("Andrew Webster")
+            assertThat(article.title).isEqualTo("Star Wars spinoff Andor has a new trailer and a new premiere date - The Verge")
+        }
+    }
+
+    @After
+    fun tearDown() {
+        server.shutdown()
+    }
+
+}
