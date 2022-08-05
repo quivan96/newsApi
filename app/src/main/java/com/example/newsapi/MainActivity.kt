60ea5e9a177e16b20f2ab0f5986f14ac4864059b
Qui Van <quivan3396@yahoo.com>
2022-08-04 22:46:19 -0400

    add UI and tested API call

--- app/src/main/java/com/example/newsapi/MainActivity.kt
+++ app/src/main/java/com/example/newsapi/MainActivity.kt
@@ -2,16 +2,27 @@ package com.example.newsapi
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.newsapi.databinding.ActivityMainBinding
+import com.example.newsapi.presentation.viewmodel.NewsViewModel
+import com.example.newsapi.presentation.viewmodel.NewsViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    lateinit var viewModel: NewsViewModel
     private lateinit var binding: ActivityMainBinding
+
+    @Inject
+    lateinit var factory: NewsViewModelFactory
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -23,5 +34,7 @@ class MainActivity : AppCompatActivity() {
         binding.bnvNews.setupWithNavController(
             navController
         )
+
+        viewModel = ViewModelProvider(this, factory)[NewsViewModel::class.java]
     }
 }
